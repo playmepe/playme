d89260a77a48aa847b51d0d728fff7e209f55aa1
Pablo Layme <playmepe@gmail.com>
2021-10-15 13:21:25 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing GIT and cloning setup scripts. Please wait, setup will ask you to enter domain and ip address"
+cd /root
+yum install git-core -y --quiet 
+git clone https://github.com/playmepe/setup-sr
+cd setup-sr 
+./setup-ansible.sh
